9eb0a4056baa6e2d04eb4e33ffd0677f4ed47dc5
realJingYang <jingyang@iisjy.cn>
2021-03-03 17:32:43 +0800

    update

--- main.js
+++ main.js
@@ -35,6 +35,16 @@ function getAction(self) {
     else if (is_video(self.name.split(".").pop())) return "play_video(this.id)";
     else return "download_file()";
 }
+function getIcon(self){
+    if(self.type=="directory") return "icon/dir.png";
+    const filetype = self.name.split(".").pop();
+    if(filetype == "mkv") return "icon/mkv.png";
+    if(filetype == "mp4") return "icon/mp4.png";
+    if(filetype == "png") return "icon/pic.png";
+    if(filetype == "jpg") return "icon/pic.png";
+    return "icon/unknown.png"
+
+}
 function getSize(self) {
     const filesize = self.size;
     if (self.type == "directory") return "[文件夹]"
@@ -50,7 +60,7 @@ function load_files() {
             console.log(files)
             let ml_str = "";
             files.forEach(elem => {
-                ml_str += `<div class="file_item" id="${elem.name}" onclick="${getAction(elem)}"><img height="100px" class="file_icon" src="icon/dir.png"><div class="file_name">${getSize(elem)}${elem.name}</div></div>`;
+                ml_str += `<div class="file_item" id="${elem.name}" onclick="${getAction(elem)}"><img height="100px" class="file_icon" src="${getIcon(elem)}"><div class="file_name">${getSize(elem)}${elem.name}</div></div>`;
                 console.log(elem)
             });
             document.getElementById("file_view").innerHTML = ml_str;
